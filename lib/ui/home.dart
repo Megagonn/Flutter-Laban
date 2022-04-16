b084b23e6f0aa35eb77be6d38b10fa40283188f0
Meg <yusuffrasheed.yr@gmail.com>
2022-04-16 13:32:16 +0100

    hotsales panel optimised

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:laban/color.dart';
 import 'package:laban/model/model.product.dart';
-import 'package:laban/utilities/cat.utilities.dart';
+import 'package:laban/utilities/cat.utl.dart';
+import 'package:laban/utilities/hotsales.utl.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -155,58 +156,7 @@ class _HomeState extends State<Home> {
                           for (var i = 0; i < data.length; i++) {
                             var map = Product.toMap(data[i]);
                             list.add(map.discount
-                                ? Container(
-                                    padding: const EdgeInsets.all(8),
-                                    margin: const EdgeInsets.only(right: 5),
-                                    decoration: BoxDecoration(
-                                        color: Colors.grey.shade400,
-                                        borderRadius:
-                                            BorderRadius.circular(15)),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Flexible(
-                                          child: Container(
-                                            padding: const EdgeInsets.all(2),
-                                            decoration: BoxDecoration(
-                                                color: white,
-                                                borderRadius:
-                                                    BorderRadius.circular(30)),
-                                            child: Text(
-                                              "Free shiping",
-                                              style: TextStyle(color: primary),
-                                            ),
-                                          ),
-                                        ),
-                                        Center(
-                                          child: Container(
-                                            height: 100,
-                                            margin: const EdgeInsets.all(10),
-                                            child: Image.network(map.pics),
-                                          ),
-                                        ),
-                                        Flexible(
-                                          child: Text(
-                                            map.name,
-                                            // style: const TextStyle(
-                                            //     fontWeight: FontWeight.bold),
-                                          ),
-                                        ),
-                                        Flexible(
-                                          child: Container(
-                                              margin:
-                                                  const EdgeInsets.only(top: 4),
-                                              child: Text(
-                                                "#${map.price}",
-                                                style: const TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              )),
-                                        ),
-                                      ],
-                                    ),
-                                  )
+                                ? HotSales(map: map)
                                 : const SizedBox.shrink());
                           }
                           // print(list);
